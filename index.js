4e57dc3a63f335e623349975ff8d0a5298b6fd88
Connie Standish <conniestand@gmail.com>
2022-06-07 21:43:23 -0400

    created variables

---
+++ index.js
@@ -0,0 +1,14 @@
+//Declare variables
+//Access data in form by pressing add button or enter key
+//Typed item must appear in ul#todo-list element below form
+//Create li element
+//Create button element
+//Append item in a button element nested in li element
+//Use form submit event
+
+var form = document.querySelector('form')
+var buttonElement = document.createElement('button')
+document.body.appendChild(buttonElement)
+var liElement = document.createElement('li')
+document.body.appendChild(liElement)
+
